e7da881335523cdc8966c6f783b7abc039df0018
soniafaradella <92154486+soniafaradella@users.noreply.github.com>
2021-11-20 16:48:12 +0700

    Update registrasi.php

--- application/views/autentifikasi/registrasi.php
+++ application/views/autentifikasi/registrasi.php
@@ -3,6 +3,7 @@
       <div class="card o-hidden border-0 shadow-lg my-5 col-lg-7 mx-auto">
           <div class="card-body p-0">
               <!-- Nested Row within Card Body -->
+						
               <div class="row">
                   <div class="col-lg">
                       <div class="p-5">
